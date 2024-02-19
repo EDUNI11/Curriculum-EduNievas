e0de5d61cf5ecde894cf495b0bb9c7b9650c9cc7
EDUNI11 <edunievasdris@gmail.com>
2024-02-19 21:06:34 +0100

    v1. Presentacion - Curriculum

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,11 @@
 import { Routes } from '@angular/router';
+import {IndexComponent} from "./index/index.component";
+import {CurriculumComponent} from "./curriculum/curriculum.component";
+import {ProyectosComponent} from "./proyectos/proyectos.component";
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {path: '', component:IndexComponent},
+  {path: 'curriculum', component:CurriculumComponent},
+  {path: 'proyectos', component:ProyectosComponent}
+];
+
